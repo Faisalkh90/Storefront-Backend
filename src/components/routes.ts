2e2fb6a1d0a4b8a7ae5beba66df99d71a73af948
faisal <nmr.njd22@gmail.com>
2022-11-05 01:40:38 +0300

    Added endpoints

--- src/components/routes.ts
+++ src/components/routes.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express';
 import userRouter from './user/user.routes';
 import productRouter from './product/productRoutes';
-
+import userRouter1 from './user/userRoutes';
+import orderRoutes from './order/orderRoutes';
 const apiRouter = express.Router();
 
 apiRouter.use(userRouter);
 apiRouter.use(productRouter);
-
+apiRouter.use(userRouter1);
+apiRouter.use(orderRoutes);
 export default apiRouter;
