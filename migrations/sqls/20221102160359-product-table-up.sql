98e744c2f0da2141687cf2454646b45df50fcf4a
faisal <nmr.njd22@gmail.com>
2022-11-16 16:36:43 +0300

    Update tables

--- migrations/sqls/20221102160359-product-table-up.sql
+++ migrations/sqls/20221102160359-product-table-up.sql
@@ -1,5 +1,5 @@
 /* Replace with your SQL commands */
-create table product(
+create table if not exists product(
     id serial primary key,
     name varchar(255) not null,
     price integer not null,
