501a3ebfc0400f8b31e4af345460a52bc35303f4
faisal <nmr.njd22@gmail.com>
2022-11-16 22:53:41 +0300

    Update endpoints

--- src/components/routes.ts
+++ src/components/routes.ts
@@ -1,12 +1,10 @@
 import express, { Express } from 'express';
-import userRouter from './user/user.routes';
 import productRouter from './product/productRoutes';
 import userRouter1 from './user/userRoutes';
 import orderRoutes from './order/orderRoutes';
 const apiRouter = express.Router();
 
-apiRouter.use(userRouter);
 apiRouter.use('/products', productRouter);
-apiRouter.use('/userc', userRouter1);
+apiRouter.use('/user', userRouter1);
 apiRouter.use('/order', orderRoutes);
 export default apiRouter;
