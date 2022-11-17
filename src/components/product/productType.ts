0778aab7ed526b5d45000f68c97d6e5c5bae8485
faisal <nmr.njd22@gmail.com>
2022-11-17 12:21:14 +0300

    Update types

--- src/components/product/productType.ts
+++ src/components/product/productType.ts
@@ -1,16 +1,6 @@
-// export interface ICreateProduct {
-//   name: string;
-//   price: number;
-// }
-
 export interface IProduct {
   id?: number;
   name: string;
   price: number;
   description?: string;
 }
-// export interface productSerialized {
-//   id: number;
-//   name: string;
-//   price: number;
-// }
--- src/components/user/user.interfaces.ts
+++ src/components/user/user.interfaces.ts
@@ -1,24 +1,8 @@
-export interface ICreateUser{
-  firstname: string,
-  lastname: string,
-  email: string,
-  password: string
-}
-
 export interface IUser {
-  id: number,
-  firstname: string,
-  lastname: string,
-  password: string,
-  email: string,
-  created_at: string,
-}
-
-export interface IUserSerialized {
-  id: number,
-  firstname: string,
-  lastname: string,
-  password?: string,
-  email: string,
-  created_at: string,
+  id: number;
+  firstname: string;
+  lastname: string;
+  password: string;
+  email: string;
+  created_at: string;
 }
