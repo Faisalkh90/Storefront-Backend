8557bd381a3a6de9b9f5dbabd98ac647589de041
faisal <nmr.njd22@gmail.com>
2022-11-19 20:54:41 +0300

    Added token authorization to endpoint

--- src/components/order-products/order-productsRoutes.ts
+++ src/components/order-products/order-productsRoutes.ts
@@ -2,10 +2,30 @@ import express from 'express';
 import * as orderProductsController from './order-productsControllers';
 import { tokenAuthorization } from '../../middlewares/authorization';
 const orderProductsRoutes = express.Router();
-orderProductsRoutes.post('/', orderProductsController.create);
-orderProductsRoutes.get('/', orderProductsController.getAllOrderProducts);
-orderProductsRoutes.get('/:id', orderProductsController.getOneOrderProduct);
-orderProductsRoutes.patch('/:id', orderProductsController.updateOne);
-orderProductsRoutes.delete('/:id', orderProductsController.deleteOne);
+orderProductsRoutes.post(
+  '/',
+  tokenAuthorization,
+  orderProductsController.create
+);
+orderProductsRoutes.get(
+  '/',
+  tokenAuthorization,
+  orderProductsController.getAllOrderProducts
+);
+orderProductsRoutes.get(
+  '/:id',
+  tokenAuthorization,
+  orderProductsController.getOneOrderProduct
+);
+orderProductsRoutes.patch(
+  '/:id',
+  tokenAuthorization,
+  orderProductsController.updateOne
+);
+orderProductsRoutes.delete(
+  '/:id',
+  tokenAuthorization,
+  orderProductsController.deleteOne
+);
 
 export default orderProductsRoutes;
