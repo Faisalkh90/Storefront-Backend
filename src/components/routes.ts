6ab51fbfab6d1299de08c17a445b6d21fa8056df
faisal <nmr.njd22@gmail.com>
2022-11-19 20:52:51 +0300

    Added order-products endpoint

--- src/components/routes.ts
+++ src/components/routes.ts
@@ -2,9 +2,11 @@ import express, { Express } from 'express';
 import productRouter from './product/productRoutes';
 import userRouter1 from './user/userRoutes';
 import orderRoutes from './order/orderRoutes';
+import orderProductsRoutes from './order-products/order-productsRoutes';
 const apiRouter = express.Router();
 
 apiRouter.use('/products', productRouter);
 apiRouter.use('/user', userRouter1);
 apiRouter.use('/order', orderRoutes);
+apiRouter.use('/order-products', orderProductsRoutes);
 export default apiRouter;
